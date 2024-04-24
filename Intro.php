26516fea6714f78a6646f4997a46b5296a9ca923
Bhupinder Sehjal <101923266+BhupinderSehjal@users.noreply.github.com>
2024-04-24 23:27:04 +0530

    Add files via upload

---
+++ Intro.php
@@ -0,0 +1,10 @@
+<?php
+$name = "Bhupinder Kumar";
+$education = "B.Tech in Computer Science Engineering at Sant Bhag Singh University";
+$interests = "web development";
+$email = "bhupindersehjal9@gmail.com";
+
+$intro = "Hello, I'm $name, currently pursuing $education. My interests lie in $interests, and you can reach me at $email.";
+
+echo $intro;
+?>
